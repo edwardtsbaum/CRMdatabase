1b949024844841fb1d413a50faae8538d46030b5
EdwardtBaum <edwardt.s.baum@gmail.com>
2019-11-23 00:22:19 -0500

    added error and search functionality

--- routes/customers.js
+++ routes/customers.js
@@ -1,19 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const Customer = require('../models/customer'); //this is going to give us access to that customer model
 
 // all customers route
-router.get('/', (req, res) => {
-  res.render('customers/index')
+router.get('/', async (req, res) => {
+  let searchOptions = {}
+  if (req.query.name != null && req.query.name !== ' ') {
+    searchOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try{
+    const customers = await Customer.find(searchOptions);
+    res.render('customers/index', {
+      customers: customers,
+      searchOptions: req.query})
+  }catch {
+    res.redirect('/')
+  }
 });
 
 // new customer route
 router.get('/new', (req, res) => {
-  res.render('customers/new')
+  res.render('customers/new', { customer: new Customer() })
 });
 
 // create customer route
-router.post('/', (req, res) => {
-  res.send('Create')
+router.post('/', async (req, res) => {
+  const customer = new Customer({
+    name: req.body.name // we want to make sure we explicitly set the data we want to be sent to the server
+  });
+  try{
+    const newCustomer = await customer.save();
+    //res.redirect('customers/${newCustomer.id}')
+    res.redirect('customers')
+  }catch {
+    res.render('customers/new', {
+      customer: customer,
+      errorMessage: 'Error creating customer'
+    })
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
+  require('dotenv').config();
 }
 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const customerRouter = require('./routes/customers');
@@ -14,11 +15,14 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false}));
 
 const mongoose = require('mongoose');
 //connection string, cant hard code connection
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true });
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
